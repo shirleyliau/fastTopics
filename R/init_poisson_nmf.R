7312dadbc141113ffc1e098bbc795fba8537da9c
Ee Shan Liau Shirley <69098782+shirleyliau@users.noreply.github.com>
2024-12-28 15:01:32 -0500

    Update init_poisson_nmf.R to accept iter.max

--- R/init_poisson_nmf.R
+++ R/init_poisson_nmf.R
@@ -31,7 +31,7 @@
 #' 
 init_poisson_nmf <-
   function (X, F, L, k, init.method = c("topicscore","random"),
-            beta = 0.5, betamax = 0.99, control = list(),
+            beta = 0.5, betamax = 0.99, control = list(), iter.max = 100,
             verbose = c("detailed","none")) {
 
   # Check input X.
@@ -87,7 +87,7 @@ init_poisson_nmf <-
       # Initialize the factors using the "Topic SCORE" algorithm.
       if (verbose == "detailed")
         cat("Initializing factors using Topic SCORE algorithm.\n")
-      F <- tryCatch(topic_score(X,k),
+      F <- tryCatch(topic_score(X, k, iter.max),
                     error = function (e) {
                       warning("Topic SCORE failure occurred; falling back ",
                               "to init.method == \"random\"")
